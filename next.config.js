6aa7180300438cbd7ad7d25b56fe51688d2db5fa
Pieter Bergwerff <p.bergwerff@studiostaat.nl>
2019-01-05 17:42:19 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS();
-
-/*
-{
+module.exports = withCSS({
 	assetPrefix:
 		process.env.NODE_ENV === 'production' ? '/react-inline-worker' : '',
-}
-*/
+});
